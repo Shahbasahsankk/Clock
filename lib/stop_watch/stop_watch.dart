ebb77af9da987045083c8334ca3fad574cedec28
Shahbas Ahsan K K <shabum18m@gmail.com>
2022-09-25 22:21:56 +0530

    check

--- lib/stop_watch/stop_watch.dart
+++ lib/stop_watch/stop_watch.dart
@@ -64,7 +64,7 @@ class _StopWatchState extends State<StopWatch> {
                       text: ".$digitMilliSeconds",
                       style:  TextStyle(
                         fontSize: 40.sp,
-                        color: Colors.red,
+                        color: Colors.yellow,
                       ),
                     )
                   ],
